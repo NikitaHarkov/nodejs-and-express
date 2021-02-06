3b73324f594d3c7b59e323984148b8e548895749
«Nikita <mrnikita.harkov@gmail.com>
2021-02-06 15:00:20 +0200

    Feedback and Speakers services + cookie-session

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-module.exports = () => {
-  router.get('/', (req, res) => res.send('Feeback page'))
+module.exports = (params) => {
+  const { feedbackService } = params
+  router.get('/', async (req, res) => {
+    const feedbacks = await feedbackService.getData()
+    res.json(feedbacks)
+  })
 
   router.post('/:shortname', (req, res) => res.send('Feedback from posted'))
 
